16f156e0b4e5ba6879476fec551ece1d00da6edf
hewx <1825724721@qq.com>
2023-04-27 13:22:35 +0800

    0427dd

---
+++ cli/scripts/build.js
@@ -0,0 +1,46 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { spawn } from 'child_process';
+import fs from 'fs';
+import {
+  copyPackages,
+  delDir,
+  checkoutDir,
+} from './utils.js';
+
+const startBuild = async () => {
+  const CurrentPath = path.dirname(fileURLToPath(import.meta.url));
+
+  const getPath = (pathd) => path.resolve(CurrentPath, pathd);
+
+  const vue2Package = {
+    srcDir: getPath('../../for-vue2/src/packages'),
+    destDir: getPath('../../dist/npm/for-vue2'),
+  };
+
+  const vue3Package = {
+    srcDir: getPath('../../for-vue3/src/packages'),
+    destDir: getPath('../../dist/npm/for-vue3'),
+  };
+
+  checkoutDir(vue2Package.destDir);
+  checkoutDir(vue3Package.destDir);
+
+  delDir(vue2Package.destDir);
+  delDir(vue3Package.destDir);
+
+  copyPackages(vue2Package.srcDir, vue2Package.destDir);
+  copyPackages(vue3Package.srcDir, vue3Package.destDir);
+
+  const vue2PackagePath = path.resolve(CurrentPath, '../../for-vue2/node_modules/vue');
+  const newVue2PackagePath = path.resolve(CurrentPath, '../../for-vue2/node_modules/vue_old');
+  if (fs.existsSync(vue2PackagePath)) {
+    fs.renameSync(vue2PackagePath, newVue2PackagePath);
+  }
+
+  spawn('yarn vitepress build', { shell: true, stdio: 'inherit' });
+
+  fs.renameSync(newVue2PackagePath, newVue2PackagePath);
+};
+
+startBuild();
--- for-vue2/vue.config.js
+++ for-vue2/vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   pluginOptions: {
     'h-open-devtools': {
-      'mp-weixin': 'E:\\微信web开发者工具',
+      'mp-weixin': 'D:\\wechatDev\\微信web开发者工具',
     },
   },
 };
