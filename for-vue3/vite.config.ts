2e38b13d2f22f37e447c5026617ac91157b34014
hewx815 <1825724721@qq.com>
2023-12-03 14:04:46 +0800

    1

--- for-vue3/vite.config.ts
+++ for-vue3/vite.config.ts
@@ -3,41 +3,38 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import uni from '@dcloudio/vite-plugin-uni';
 import { fileURLToPath } from 'url';
-import {
-  hUniBuild,
-  defineConfigHUniBuild,
-} from './src/packages/plugins/vite-plugin-h-uni-build';
+// import {
+//   hUniBuild,
+//   defineConfigHUniBuild,
+// } from './src/packages/plugins/vite-plugin-h-uni-build';
 // eslint-disable-next-line import/no-relative-packages
-import { startAPPDevTool } from '../for-vue2/src/packages/plugins/APPDevTool';
+// import { startAPPDevTool } from '../for-vue2/src/packages/plugins/APPDevTool';
 
 const CurrentPath = path.dirname(fileURLToPath(import.meta.url));
 
-const config = defineConfigHUniBuild({
-  delOldFile: true,
-  setMode: false,
-  openDevTools: {
-    paths: {
-      'mp-weixin': 'D:\\wechatDev\\微信web开发者工具',
-    },
-  },
-  async afterBuild() {
-    if (process.env.UNI_PLATFORM === 'app') {
-      await startAPPDevTool({ devAndroid: true, root: '../' });
-    }
-    if (process.env.UNI_PLATFORM === 'h5' && process.env.NODE_ENV === 'production') {
-      process.exit(0);
-    }
-  },
-});
+// const config = defineConfigHUniBuild({
+//   delOldFile: true,
+//   setMode: false,
+//   openDevTools: {
+//     paths: {
+//       'mp-weixin': 'D:\\wechatDev\\微信web开发者工具',
+//     },
+//   },
+//   async afterBuild() {
+//     if (process.env.UNI_PLATFORM === 'app') {
+//       await startAPPDevTool({ devAndroid: true, root: '../' });
+//     }
+//     if (process.env.UNI_PLATFORM === 'h5' && process.env.NODE_ENV === 'production') {
+//       process.exit(0);
+//     }
+//   },
+// });
 
 export default defineConfig({
-  plugins: [uni(), hUniBuild(config)],
+  plugins: [uni()], // hUniBuild(config)
   server: {
     port: 8080,
     host: 'localhost',
-    fs: {
-      strict: false,
-    },
   },
   resolve: {
     alias: {
